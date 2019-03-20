45d3701d6cb7cd0396789b7516df0f649b5369ec
Jamie Gullbrand <jamiegullbrand@gmail.com>
2019-03-20 11:01:08 -0400

    data files, py file, readme updated

---
+++ README.md
@@ -0,0 +1,39 @@
+## Data analysis & visualization - House of Representatives expenditures
+
+I analyzed expenses for members of the US House of Representatives to see how priorities adjusted during an election year (2018). As shown below, during the start of the reelection period (Q3 of 2018) the expense priorities shifted. The three categories that had the most significant increase from Q1 to Q3 were 1) Printing & Reproduction, 2) Franked Mail (mail to their constituents) and 3) Travel. This shift in priorities is due to the election year where the US House Members are focused on getting reelected for another 2 years.
+
+The data I used for this project is from projects.propublica.org, which is based on expenditure reports published directly from the House. I downloaded the three most current data files for 2018 (Q1-2018, Q2-2018, Q3-2018). Unfortunately, data for Q4 2018 is not yet available. It would have been helpful to have Q4 data to analyze if the trends I outlined continued through election day. 
+
+After organizing the data, I created a pie chart to give an overview of expenses categories. The top three categories include: 
+- Personnel compensation (accounts for over 75% of total expenses)
+- Rent, Communication, Utilities
+- Travel 
+
+![chart_expense_categories](https://user-images.githubusercontent.com/40340806/54649502-db7a3780-4a80-11e9-8eda-fa5f9102c39e.png)
+
+
+The code snippet below shows how I merged the Q1 & Q3 data, found the percent increase for each cateogory, and then cleaned up the column names and sorted the data. The table below shows a cleaned up version of the output.
+
+```python
+comparison_Q1_Q3 = q1_data.merge(q3_data, on='CATEGORY', suffixes=['_Q1', '_Q3'])
+comparison_Q1_Q3['percent_increase'] = comparison_Q1_Q3.apply(lambda row: round((row.AMOUNT_Q3 - row.AMOUNT_Q1)/row.AMOUNT_Q1,3), axis=1)
+
+percent_increase_df = comparison_Q1_Q3[['CATEGORY', 'AMOUNT_Q1', 'AMOUNT_Q3', 'percent_increase']]
+percent_increase_df.columns = ["Expense Category", "Q1 Total Expenses", "Q3 Total Expenses", "% Increase in Total Expenses"]
+percent_increase_df = percent_increase_df.sort_values(by=['% Increase in Total Expenses'], ascending=False).reset_index(drop=True)
+```
+
+This table shows the % change in total expenses from Q1 to Q3. The top three expense cateogires (based on % change in the total expenses) are 1) Printing & Reproduction, 2) Franked Mail (mail to their constituents) and 3) Travel
+
+![Screen Shot 2019-03-20 at 9 19 28 AM](https://user-images.githubusercontent.com/40340806/54687329-b540b000-4af1-11e9-8ae1-7c039b000536.png)
+
+
+This table shows the how the individual expense cateogry each represent a portion of the total expenses from Q1 to Q3. The top three expense cateogires (based on overall % change in the total propotion of expenses) are 1) Printing & Reproduction, 2) Franked Mail (mail to their constituents) and 3) Travel
+
+![Screen Shot 2019-03-20 at 9 55 03 AM](https://user-images.githubusercontent.com/40340806/54689647-55003d00-4af6-11e9-8892-2f4ef8dc0366.png)
+
+
+### Top Three Spenders - Q3 2018 (Leading up to the 2018 Midterm Elections)**
+The top 3 spenders for Quarter 3 in 2018 were Tom MacArthur, Steve Knight, Randy Hultgren. All three of these former congressman had a very tight race in their elections, but all ended up losing. These very close reelection campaigns in Q3 2018 account for the spike in expenses.
+
+![Screen Shot 2019-03-20 at 10 15 13 AM](https://user-images.githubusercontent.com/40340806/54691380-86c6d300-4af9-11e9-9111-26c10754d38d.png)
---
+++ house_expenses_2018.py
@@ -0,0 +1,79 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import glob
+
+files = glob.glob('data_files/2018*.csv')
+
+data_files = []
+for file in files:
+	data = pd.read_csv(file)
+	data_files.append(data)
+
+data_2018 = pd.concat(data_files).reset_index(drop=True)	
+#print(data_2018.head()) # prints first 5 rows
+
+#Totals Grouped by member, expense category and quarters
+member_totals = data_2018.groupby('OFFICE').AMOUNT.sum() # gives the total expenses for each member of the House
+category_totals = data_2018.groupby('CATEGORY').AMOUNT.sum().reset_index() # gives the total expenses for each category
+quarter_totals = data_2018.groupby('QUARTER').AMOUNT.sum() # gives the total expenses for each quarter
+
+#Data filtered by Quarter
+q1_data = data_2018[data_2018.QUARTER == 'Q1'].groupby('CATEGORY').AMOUNT.sum().reset_index()
+q2_data = data_2018[data_2018.QUARTER == 'Q2'].groupby('CATEGORY').AMOUNT.sum().reset_index()
+q3_data = data_2018[data_2018.QUARTER == 'Q3'].groupby('CATEGORY').AMOUNT.sum().reset_index()
+
+#Pie Chart for category totals
+sort_category_totals = category_totals.sort_values(by=['AMOUNT'], ascending=False)
+plt.pie(sort_category_totals['AMOUNT'])
+plt.legend(sort_category_totals['CATEGORY'], loc = "upper left")
+plt.axis('equal')
+plt.title('Top Expense Categories for the US House of Reps (Q1, Q2, Q3 2018)')
+plt.show() # displays chart
+plt.close('all') # this clears out the previous plot in case I decide to include other charts below
+
+# table_Q1_Q3_increase.csv - Amounts for Q1 and Q3 and the percentage increase
+comparison_Q1_Q3 = q1_data.merge(q3_data, on='CATEGORY', suffixes=['_Q1', '_Q3'])
+comparison_Q1_Q3['percent_increase'] = comparison_Q1_Q3.apply(lambda row: round((row.AMOUNT_Q3 - row.AMOUNT_Q1)/row.AMOUNT_Q1,3), axis=1)
+
+percent_increase_df = comparison_Q1_Q3[['CATEGORY', 'AMOUNT_Q1', 'AMOUNT_Q3', 'percent_increase']]
+percent_increase_df.columns = ["Expense Category", "Q1 Total Expenses", "Q3 Total Expenses", "% Increase in Total Expenses"] #renaming the columns
+percent_increase_df = percent_increase_df.sort_values(by=['% Increase in Total Expenses'], ascending=False).reset_index(drop=True)
+percent_increase_df.to_csv('table_Q1_Q3_increase.csv')
+
+
+# table_Q1_Q3_expense_proportions.csv - The proportion of expenses based on categories from Q1 to Q3
+q1_sum = q1_data.AMOUNT.sum()
+def q1_calc_percentage(num):
+	return round(num/q1_sum,3)
+q1_data['Q1_percentage_of_total'] = q1_data['AMOUNT'].apply(q1_calc_percentage)
+
+q3_sum = q3_data.AMOUNT.sum()
+def q3_calc_percentage(num):
+	return round(num/q3_sum,3)
+q3_data['Q3_percentage_of_total'] = q3_data['AMOUNT'].apply(q3_calc_percentage)
+
+comparison_Q1_Q3_percentage = q1_data.merge(q3_data, on='CATEGORY', suffixes=['_Q1', '_Q3'])
+comparison_Q1_Q3_percentage['percent_difference_of_total'] = comparison_Q1_Q3_percentage.apply(lambda row: round(row.Q3_percentage_of_total - row.Q1_percentage_of_total,3), axis=1)
+comparison_Q1_Q3_percentage = comparison_Q1_Q3_percentage[['CATEGORY', 'Q1_percentage_of_total', 'Q3_percentage_of_total', 'percent_difference_of_total']]
+comparison_Q1_Q3_percentage.columns = ["Expense Category", "Q1 - percentage of total expenses", "Q3 - percentage of total expenses", "change in proportion of expenses"] #renaming the columns
+comparison_Q1_Q3_percentage = comparison_Q1_Q3_percentage.sort_values(by=["change in proportion of expenses"], ascending=False).reset_index(drop=True)
+comparison_Q1_Q3_percentage.to_csv('table_Q1_Q3_expense_proportions.csv')
+
+
+# Top 3 Spenders (cumulative of Q1, Q2, Q3)
+top_member_totals = data_2018.groupby('OFFICE').AMOUNT.sum().reset_index()
+top_three_spenders = top_member_totals.sort_values(by=['AMOUNT'], ascending=False).head(3).reset_index()
+
+
+# Top 3 spenders (Based on Q3)
+q1_top_member_total = data_2018[data_2018.QUARTER == 'Q1'].groupby('OFFICE').AMOUNT.sum().reset_index()
+q3_top_member_total = data_2018[data_2018.QUARTER == 'Q3'].groupby('OFFICE').AMOUNT.sum().reset_index()
+merged_data_Q1_Q3 = q1_top_member_total.merge(q3_top_member_total, on='OFFICE', suffixes=['_Q1', '_Q3'])
+merged_data_Q1_Q3 = merged_data_Q1_Q3[['OFFICE', 'AMOUNT_Q1', 'AMOUNT_Q3']] # removing other columns
+merged_data_Q1_Q3['total_amount_increase'] = merged_data_Q1_Q3.apply(lambda row: round((row.AMOUNT_Q3 - row.AMOUNT_Q1),3), axis=1)
+merged_data_Q1_Q3['percent_increase'] = merged_data_Q1_Q3.apply(lambda row: round((row.AMOUNT_Q3 - row.AMOUNT_Q1)/row.AMOUNT_Q1,3), axis=1)
+top_three_spenders_q3 = merged_data_Q1_Q3.sort_values(by=['AMOUNT_Q3'], ascending=False).head(3).reset_index(drop=True)
+top_three_spenders_q3.to_csv('Q3_top_three_spenders.csv')
+print(top_three_spenders_q3)
+
+
